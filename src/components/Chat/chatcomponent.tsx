6a4afe8461fdfc0a6652936d084dc6dd93ee2699
Weerawat Wongmanit <weerawatwmt@gmail.com>
2024-04-30 12:20:23 +0700

    Update chatcomponent.tsx

--- src/components/Chat/chatcomponent.tsx
+++ src/components/Chat/chatcomponent.tsx
@@ -1,8 +1,17 @@
 import { Session, Chatbox } from "@talkjs/react";
+import { useSession } from "next-auth/react";
+import { api } from "~/utils/api";
 
 function ChatComponent() {
+  const sessionId = useSession().data?.user.id ?? "NO_OP";
+  const { data } = api.user.getUserById.useQuery(sessionId);
+
+  // Determine userId based on user's role
+  const userId =
+    data?.role === "admin" ? "sample_user_alice" : "sample_user_sebastian";
+
   return (
-    <Session appId="tKpCN1ok" userId="sample_user_alice">
+    <Session appId="tKpCN1ok" userId={userId}>
       <Chatbox
         conversationId="sample_conversation"
         style={{ width: "100%", height: "500px" }}

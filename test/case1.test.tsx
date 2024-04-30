4e3a6c9afbd8142353ab028a5d51533ae337cb78
HRNPH <hrnph@protonmail.com>
2024-04-30 20:13:15 +0700

    feat: test mock

--- test/case1.test.tsx
+++ test/case1.test.tsx
@@ -4,51 +4,32 @@ import { render, screen } from "@testing-library/react";
 import { SessionProvider } from "next-auth/react";
 import Navbar from "~/pages/layout/page/Nav";
 
-jest.mock("next/router", () => ({
-  useRouter() {
-    return {
-      pathname: "/",
-    };
-  },
-}));
+// mock Database
+import { MockContext, Context, createMockContext } from "./context";
+import { Session } from "next-auth";
+import { createInnerTRPCContext, createTRPCContext } from "~/server/api/trpc";
+import { appRouter } from "~/server/api/root";
+import { initTRPC } from "@trpc/server";
 
-jest.mock("../src/utils/api", () => ({
-  api: {
-    user: {
-      getUserById: {
-        useQuery: jest.fn(() => ({
-          data: {
-            user: {
-              id: "1",
-              name: "Alice",
-            },
-          },
-          isLoading: false,
-          isError: false,
-        })),
-      },
-    },
-  },
-}));
+describe("Wishlist Companies", () => {
+  let mockCtx: MockContext;
+  let prisma_ctx: Context;
 
-describe("Navbar and links", () => {
-  it("Navbar links to correct page", () => {
-    render(
-      <SessionProvider session={null}>
-        <Navbar />
-      </SessionProvider>,
-    );
-
-    const Home = screen.getByText("Home");
-    expect(Home).toBeInTheDocument();
-    expect(Home).toHaveAttribute("href", "/");
-
-    const Booking = screen.getByText("Booking");
-    expect(Booking).toBeInTheDocument();
-    expect(Booking).toHaveAttribute("href", "/booking");
-
-    const Profile = screen.getByText("Profile");
-    expect(Profile).toBeInTheDocument();
-    expect(Profile).toHaveAttribute("href", "/profile");
+  beforeEach(() => {
+    mockCtx = createMockContext();
+    prisma_ctx = mockCtx as unknown as Context;
   });
+
+  // session
+  const session: Session = {
+    user: {
+      id: "test",
+      firstName: "John", // Add the missing property 'firstName'
+      lastName: "Doe",
+      email: "test@testing.com",
+    },
+    expires: "1",
+  };
+
+  it("User can add company to wishlist", async () => {});
 });
